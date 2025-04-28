d53041b0870c41ed5e07bf9194b7e439726b277e
Josue Mullo <josue.mullo@udla.edu.ec>
2025-04-28 07:14:22 -0500

    Add project files.

---
+++ Evalucacion 1er Progreso-Josue Mullo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Evalucacion_1er_Progreso_Josue_Mullo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
